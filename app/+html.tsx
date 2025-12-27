cb06e2261406ac53ab60191735b6abe39ae816bc
Adrien DONOT <adriendonot@mbp-de-adrien.home>
2025-12-27 19:03:07 +0100

    Update manifest and HTML to use correct icon file names

--- app/+html.tsx
+++ app/+html.tsx
@@ -21,15 +21,14 @@ export default function Root({ children }: PropsWithChildren) {
         <meta name="description" content="Générez de superbes illustrations botaniques avec l'IA" />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="theme-color" content="#2d5016" />
+        <meta name="theme-color" content="#4CAF50" />
         
         {/* iOS Icons */}
         <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         
         {/* Favicon */}
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
         <link rel="shortcut icon" href="/favicon.ico" />
         
         {/* PWA Manifest */}
